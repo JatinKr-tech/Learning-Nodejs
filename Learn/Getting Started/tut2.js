e7b2b5c483d2bb952ddff85d8d547f56a9be824b
Jatin Kumar <woitot9@gmail.com>
2026-08-16 21:14:46 +0530

    docs: 'How much JavaScript do you need to know to use Node.js?';

---
+++ Learn/Getting Started/tut2.js
@@ -0,0 +1,32 @@
+//How much JavaScript do you need to know to use Node.js
+
+//What is recommended to learn before diving deep with Node.js?
+
+// Lexical Structure
+// Expressions
+// Data Types
+// Classes
+// Variables
+// Functions
+// this operator
+// Arrow Functions
+// Loops
+// Scopes
+// Arrays
+// Template Literals
+// Strict Mode
+// ECMAScript 2015 (ES6) and beyond
+// Asynchronous JavaScript
+
+//..........................
+
+//Asynchronous Programming
+
+// The following concepts are also key to understand asynchronous programming, which is one of the fundamental parts of Node.js:
+
+// Asynchronous programming and callbacks
+// Timers
+// Promises
+// Async and Await
+// Closures
+// The Event Loop
