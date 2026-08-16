6635c9f6cc28702d318e9af3d1665dc0384b2ed9
Jatin Kumar <woitot9@gmail.com>
2026-08-16 21:15:21 +0530

    docs: 'Differences between Node.js and the Browser';

---
+++ Learn/Getting Started/tut3.js
@@ -0,0 +1,15 @@
+//Differences between Node.js and the Browser
+
+//Node.js apps bring with them a huge advantage: the comfort of programming everything - the frontend and the backend - in a single language.
+
+//...................................
+
+//What changes is the ecosystem.
+
+//You control javascript version when you use Node
+
+//Browsers are slow to upgrade javascript version so you have to make your code browser compatible with technologies like Babel to transform your code, but in Node you don't have this headache.
+
+//Another difference is that Node.js supports both the CommonJS and ES module systems (since Node.js v12), while in the browser, we are starting to see the ES Modules standard being implemented.
+
+// In practice, this means that you can use both require() and import in Node.js, while you are limited to import in the browser.
